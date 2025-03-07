f78fc1099b9f0b1a76b6ac9b318adfbc0ed0b1d5
Luke <luke.wangai@student.moringaschool.com>
2025-03-07 22:15:15 +0300

    Full of string now

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+ const currentUser = 'Onegai';
+
+ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+ const excitedWelcomeMessage = welcomeMessage .toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,3)}!`
+
+
+
+
+ 
